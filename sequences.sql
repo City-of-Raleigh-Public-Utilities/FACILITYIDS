4e890a30cd3d62bd7aff63731e8eb775a88afc93
cwhite911 <smortopahri@gmail.com>
2015-08-10 21:01:52 -0400

    Added seqences and triggers to db to replace nightly script

---
+++ sequences.sql
@@ -0,0 +1,141 @@
+--Sewer--------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------
+
+CREATE SEQUENCE ssCleanout_seq
+ START WITH     148459
+ INCREMENT BY   1;
+
+---------------------------------------------------------------------------------------
+
+CREATE SEQUENCE ssSystemValve_seq
+ START WITH     10372
+ INCREMENT BY   1;
+
+---------------------------------------------------------------------------------------
+
+CREATE SEQUENCE ssControlValve_seq
+ START WITH     10599
+ INCREMENT BY   1;
+
+---------------------------------------------------------------------------------------
+
+CREATE SEQUENCE ssFitting_seq
+ START WITH     135170
+ INCREMENT BY   1;
+
+---------------------------------------------------------------------------------------
+
+CREATE SEQUENCE ssForceMain_seq
+ START WITH     11687
+ INCREMENT BY   1;
+
+---------------------------------------------------------------------------------------
+
+CREATE SEQUENCE ssGravityMain_seq
+ START WITH     169248
+ INCREMENT BY   1;
+
+---------------------------------------------------------------------------------------
+
+CREATE SEQUENCE ssLateralLine_seq
+ START WITH     148375
+ INCREMENT BY   1;
+
+---------------------------------------------------------------------------------------
+
+CREATE SEQUENCE ssAerial_seq
+ START WITH     10800
+ INCREMENT BY   1;
+
+---------------------------------------------------------------------------------------
+
+CREATE SEQUENCE ssGreaseTrap_seq
+ START WITH     10115
+ INCREMENT BY   1;
+
+---------------------------------------------------------------------------------------
+
+CREATE SEQUENCE ssNetworkStructure_seq
+ START WITH     1158
+ INCREMENT BY   1;
+
+---------------------------------------------------------------------------------------
+
+CREATE SEQUENCE ssCasing_seq
+ START WITH     10351
+ INCREMENT BY   1;
+
+---------------------------------------------------------------------------------------
+
+CREATE SEQUENCE ssManhole_seq
+ START WITH     169486
+ INCREMENT BY   1;
+
+---------------------------------------------------------------------------------------
+
+--Water--------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------
+
+CREATE SEQUENCE wControlValve_seq
+ START WITH     107610
+ INCREMENT BY   1;
+
+---------------------------------------------------------------------------------------
+
+CREATE SEQUENCE wFitting_seq
+ START WITH     192642
+ INCREMENT BY   1;
+
+---------------------------------------------------------------------------------------
+
+CREATE SEQUENCE wGravityMain_seq
+ START WITH     100082
+ INCREMENT BY   1;
+
+---------------------------------------------------------------------------------------
+
+CREATE SEQUENCE wLateralLine_seq
+ START WITH     180674
+ INCREMENT BY   1;
+
+---------------------------------------------------------------------------------------
+
+CREATE SEQUENCE wServiceConnection_seq
+ START WITH     286767
+ INCREMENT BY   1;
+
+---------------------------------------------------------------------------------------
+
+CREATE SEQUENCE wSystemValve_seq
+ START WITH     168466
+ INCREMENT BY   1;
+
+---------------------------------------------------------------------------------------
+
+CREATE SEQUENCE wPressureMain_seq
+ START WITH     206219
+ INCREMENT BY   1;
+
+---------------------------------------------------------------------------------------
+
+CREATE SEQUENCE wHydrant_seq
+ START WITH     128609
+ INCREMENT BY   1;
+
+---------------------------------------------------------------------------------------
+
+CREATE SEQUENCE wSampleStation_seq
+ START WITH     10256
+ INCREMENT BY   1;
+
+---------------------------------------------------------------------------------------
+
+CREATE SEQUENCE wCasing_seq
+ START WITH     10199
+ INCREMENT BY   1;
+
+---------------------------------------------------------------------------------------
+
+CREATE SEQUENCE wNetworkStructure_seq
+ START WITH     10074
+ INCREMENT BY   1;
---
+++ triggers.sql
@@ -0,0 +1,258 @@
+--Sewer--------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------
+
+CREATE OR REPLACE TRIGGER RPUD.ssCleanout_facilityid
+  BEFORE INSERT ON RPUD.A6889
+  FOR EACH ROW
+  BEGIN
+    IF :new.FACILITYID IS NULL THEN
+      SELECT CONCAT('SCO', ssCleanout_seq.nextval) INTO :new.FACILITYID FROM DUAL;
+    END IF;
+  END;
+/
+---------------------------------------------------------------------------------------
+
+CREATE OR REPLACE TRIGGER RPUD.ssSysValve_facilityid
+  BEFORE INSERT ON RPUD.A6789
+  FOR EACH ROW
+  BEGIN
+    IF :new.FACILITYID IS NULL THEN
+      SELECT CONCAT('SSV', ssSystemValve_seq.nextval) INTO :new.FACILITYID FROM DUAL;
+    END IF;
+  END;
+/
+---------------------------------------------------------------------------------------
+
+CREATE OR REPLACE TRIGGER RPUD.ssContValve_facilityid
+  BEFORE INSERT ON RPUD.A6799
+  FOR EACH ROW
+  BEGIN
+    IF :new.FACILITYID IS NULL THEN
+      SELECT CONCAT('SCV', ssControlValve_seq.nextval) INTO :new.FACILITYID FROM DUAL;
+    END IF;
+  END;
+/
+---------------------------------------------------------------------------------------
+
+CREATE OR REPLACE TRIGGER RPUD.ssFitting_facilityid
+  BEFORE INSERT ON RPUD.A6783
+  FOR EACH ROW
+  BEGIN
+    IF :new.FACILITYID IS NULL THEN
+      SELECT CONCAT('SF', ssFitting_seq.nextval) INTO :new.FACILITYID FROM DUAL;
+    END IF;
+  END;
+/
+---------------------------------------------------------------------------------------
+
+CREATE OR REPLACE TRIGGER RPUD.ssForceMain_facilityid
+  BEFORE INSERT ON RPUD.A6784
+  FOR EACH ROW
+  BEGIN
+    --IF :new.FACILITYID IS NULL THEN
+      SELECT CONCAT('SFMN', ssForceMain_seq.nextval) INTO :new.FACILITYID FROM DUAL;
+    --END IF;
+  END;
+/
+---------------------------------------------------------------------------------------
+
+CREATE OR REPLACE TRIGGER RPUD.ssGravityMain_facilityid
+  BEFORE INSERT ON RPUD.A6785
+  FOR EACH ROW
+  BEGIN
+    --IF :new.FACILITYID IS NULL THEN
+      SELECT CONCAT('SGMN', ssGravityMain_seq.nextval) INTO :new.FACILITYID FROM DUAL;
+    --END IF;
+  END;
+/
+---------------------------------------------------------------------------------------
+--Change A Tables
+
+CREATE OR REPLACE TRIGGER RPUD.ssLateralLine_facilityid
+  BEFORE INSERT ON RPUD.A6787
+  FOR EACH ROW
+  BEGIN
+    --IF :new.FACILITYID IS NULL THEN
+      SELECT CONCAT('SLAT', ssLateralLine_seq.nextval) INTO :new.FACILITYID FROM DUAL;
+    --END IF;
+  END;
+/
+---------------------------------------------------------------------------------------
+
+CREATE OR REPLACE TRIGGER RPUD.ssAerial_facilityid
+  BEFORE INSERT ON RPUD.A6793
+  FOR EACH ROW
+  BEGIN
+    IF :new.FACILITYID IS NULL THEN
+      SELECT CONCAT('SAM', ssAerial_seq.nextval) INTO :new.FACILITYID FROM DUAL;
+    END IF;
+  END;
+/
+---------------------------------------------------------------------------------------
+
+CREATE OR REPLACE TRIGGER RPUD.ssGreaseTrap_facilityid
+  BEFORE INSERT ON RPUD.A6786
+  FOR EACH ROW
+  BEGIN
+    IF :new.FACILITYID IS NULL THEN
+      SELECT CONCAT('SGT', ssGreaseTrap_seq.nextval) INTO :new.FACILITYID FROM DUAL;
+    END IF;
+  END;
+/
+---------------------------------------------------------------------------------------
+
+CREATE OR REPLACE TRIGGER RPUD.ssNetStruct_facilityid
+  BEFORE INSERT ON RPUD.A6791
+  FOR EACH ROW
+  BEGIN
+    IF :new.FACILITYID IS NULL THEN
+      SELECT CONCAT('SNS', ssNetworkStructure_seq.nextval) INTO :new.FACILITYID FROM DUAL;
+    END IF;
+  END;
+/
+---------------------------------------------------------------------------------------
+
+CREATE OR REPLACE TRIGGER RPUD.ssCasing_facilityid
+  BEFORE INSERT ON RPUD.A6792
+  FOR EACH ROW
+  BEGIN
+    IF :new.FACILITYID IS NULL THEN
+      SELECT CONCAT('SCA', ssCasing_seq.nextval) INTO :new.FACILITYID FROM DUAL;
+    END IF;
+  END;
+/
+---------------------------------------------------------------------------------------
+
+CREATE OR REPLACE TRIGGER RPUD.ssManhole_facilityid
+  BEFORE INSERT ON RPUD.A6788
+  FOR EACH ROW
+  BEGIN
+    IF :new.FACILITYID IS NULL THEN
+      SELECT CONCAT('SMH', ssManhole_seq.nextval) INTO :new.FACILITYID FROM DUAL;
+    END IF;
+  END;
+/
+---------------------------------------------------------------------------------------
+
+--Water--------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------
+
+CREATE OR REPLACE TRIGGER RPUD.wControlValve_facilityid
+  BEFORE INSERT ON RPUD.A6785
+  FOR EACH ROW
+  BEGIN
+    IF :new.FACILITYID IS NULL THEN
+      SELECT CONCAT('WCV', wControlValve_seq.nextval) INTO :new.FACILITYID FROM DUAL;
+    END IF;
+  END;
+/
+--------------------------------------------------------------------------------------
+
+CREATE OR REPLACE TRIGGER RPUD.wFitting_facilityid
+  BEFORE INSERT ON RPUD.A7289
+  FOR EACH ROW
+  BEGIN
+    IF :new.FACILITYID IS NULL THEN
+      SELECT CONCAT('WFIT', wFitting_seq.nextval) INTO :new.FACILITYID FROM DUAL;
+    END IF;
+  END;
+/
+--------------------------------------------------------------------------------------
+
+CREATE OR REPLACE TRIGGER RPUD.wGravityMain_facilityid
+  BEFORE INSERT ON RPUD.A7289
+  FOR EACH ROW
+  BEGIN
+    --IF :new.FACILITYID IS NULL THEN
+      SELECT CONCAT('WGM', wGravityMain_seq.nextval) INTO :new.FACILITYID FROM DUAL;
+    --END IF;
+  END;
+/
+--------------------------------------------------------------------------------------
+
+CREATE OR REPLACE TRIGGER RPUD.wLateralLine_facilityid
+  BEFORE INSERT ON RPUD.A7289
+  FOR EACH ROW
+  BEGIN
+    --IF :new.FACILITYID IS NULL THEN
+      SELECT CONCAT('WLAT', wLateralLine_seq.nextval) INTO :new.FACILITYID FROM DUAL;
+    --END IF;
+  END;
+/
+--------------------------------------------------------------------------------------
+
+CREATE OR REPLACE TRIGGER RPUD.wServiceConnection_facilityid
+  BEFORE INSERT ON RPUD.A7289
+  FOR EACH ROW
+  BEGIN
+    IF :new.FACILITYID IS NULL THEN
+      SELECT CONCAT('WSC', wServiceConnection_seq.nextval) INTO :new.FACILITYID FROM DUAL;
+    END IF;
+  END;
+/
+--------------------------------------------------------------------------------------
+
+CREATE OR REPLACE TRIGGER RPUD.wSystemValve_facilityid
+  BEFORE INSERT ON RPUD.A7289
+  FOR EACH ROW
+  BEGIN
+    IF :new.FACILITYID IS NULL THEN
+      SELECT CONCAT('WSV', wSystemValve_seq.nextval) INTO :new.FACILITYID FROM DUAL;
+    END IF;
+  END;
+/
+--------------------------------------------------------------------------------------
+
+CREATE OR REPLACE TRIGGER RPUD.wPressureMain_facilityid
+  BEFORE INSERT ON RPUD.A7289
+  FOR EACH ROW
+  BEGIN
+    --IF :new.FACILITYID IS NULL THEN
+      SELECT CONCAT('WMN', wPressureMain_seq.nextval) INTO :new.FACILITYID FROM DUAL;
+    --END IF;
+  END;
+/
+--------------------------------------------------------------------------------------
+
+CREATE OR REPLACE TRIGGER RPUD.wHydrant_facilityid
+  BEFORE INSERT ON RPUD.A7289
+  FOR EACH ROW
+  BEGIN
+    IF :new.FACILITYID IS NULL THEN
+      SELECT CONCAT('WHYD', wHydrant_seq.nextval) INTO :new.FACILITYID FROM DUAL;
+    END IF;
+  END;
+/
+--------------------------------------------------------------------------------------
+
+CREATE OR REPLACE TRIGGER RPUD.wSampleStation_facilityid
+  BEFORE INSERT ON RPUD.A7289
+  FOR EACH ROW
+  BEGIN
+    IF :new.FACILITYID IS NULL THEN
+      SELECT CONCAT('WSS', wSampleStation_seq.nextval) INTO :new.FACILITYID FROM DUAL;
+    END IF;
+  END;
+/
+--------------------------------------------------------------------------------------
+
+CREATE OR REPLACE TRIGGER RPUD.wCasing_facilityid
+  BEFORE INSERT ON RPUD.A7289
+  FOR EACH ROW
+  BEGIN
+    --IF :new.FACILITYID IS NULL THEN
+      SELECT CONCAT('WCAS', wCasing_seq.nextval) INTO :new.FACILITYID FROM DUAL;
+    --END IF;
+  END;
+/
+--------------------------------------------------------------------------------------
+
+CREATE OR REPLACE TRIGGER RPUD.wNetworkStructure_facilityid
+  BEFORE INSERT ON RPUD.A7289
+  FOR EACH ROW
+  BEGIN
+    IF :new.FACILITYID IS NULL THEN
+      SELECT CONCAT('WNS', wNetworkStructure_seq.nextval) INTO :new.FACILITYID FROM DUAL;
+    END IF;
+  END;
+/
